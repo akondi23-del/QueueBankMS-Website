e274ee926c9977906dcb80a5e365059c3bb04c09
akondi23-del <akondi23@epoka.edu.al>
2026-05-17 18:51:52 +0200

    Update get_bookings.php

--- api/get_bookings.php
+++ api/get_bookings.php
@@ -5,15 +5,15 @@ $email = $_GET['email'] ?? '';
 $date  = $_GET['date']  ?? '';
 
 if ($email) {
-    // Customer: get bookings by email
+
     $stmt = $pdo->prepare("SELECT * FROM bookings WHERE email = ? ORDER BY created_at DESC");
     $stmt->execute([$email]);
 } elseif ($date) {
-    // Staff: get bookings by date
+
     $stmt = $pdo->prepare("SELECT * FROM bookings WHERE date = ? ORDER BY slot ASC");
     $stmt->execute([$date]);
 } else {
-    // Admin: get all bookings
+
     $stmt = $pdo->prepare("SELECT * FROM bookings ORDER BY created_at DESC");
     $stmt->execute();
 }
